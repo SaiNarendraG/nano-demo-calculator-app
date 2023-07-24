1aa0ee06aad480bf28f21fb7f74dd93650711dc4
Sai Narendra <50744735+SaiNarendraG@users.noreply.github.com>
2023-07-25 01:53:09 +0530

    Update server.py

--- server.py
+++ server.py
@@ -3,17 +3,17 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route("/calculator/greeting", methods=['GET'])
+@app.route("/", methods=['GET'])
 def greeting():
-    return ''
+    return 'HWelcome to the calculator app'
 
 @app.route("/calculator/add", methods=['POST'])
-def add():
-    return ''
+def add(a,b):
+    return a+b
 
 @app.route("/calculator/subtract", methods=['POST'])
-def subtract():
-    return ''
+def subtract(a,b):
+    return a-b 
 
 if __name__ == '__main__':
     app.run(port=8080,host='0.0.0.0')
